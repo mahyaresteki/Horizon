1837fcf8a8ef9e75824cb76726dfd7b3703feb0c
Mahyar Esteki <44246254+mahyaresteki@users.noreply.github.com>
2025-03-30 23:20:52 +0330

    25.3.30

--- DatabaseScripts/HorizonDatabaseCreationScript.sql
+++ DatabaseScripts/HorizonDatabaseCreationScript.sql
@@ -14,10 +14,59 @@ CREATE TABLE Basic.EducationLevel(
 	ModifierId bigint
 );
 
-CREATE TABLE Basic.MeetingType(
+CREATE TABLE Basic.Priority(
     Id serial PRIMARY KEY NOT NULL, 
     Code varchar(10) NOT NULL UNIQUE,
     Title varchar(200) NOT NULL,
+	Ordering int NOT NULL,
+	Color varchar(7) NOT NULL,
+	Description varchar(4000),
+    IsActive boolean NOT NULL DEFAULT true,
+    IsDeleted boolean NOT NULL DEFAULT false,
+    CreateDate timestamp NOT NULL DEFAULT NOW(),
+	CreatorId bigint NOT NULL,
+    ModifyDate timestamp,
+	ModifierId bigint
+);
+
+CREATE TABLE Basic.IssueType(
+    Id serial PRIMARY KEY NOT NULL, 
+    Code varchar(10) NOT NULL UNIQUE,
+    Title varchar(200) NOT NULL,
+	IconAddress varchar(1000) NOT NULL,
+	Color varchar(7) NOT NULL,
+	Description varchar(4000),
+    IsActive boolean NOT NULL DEFAULT true,
+    IsDeleted boolean NOT NULL DEFAULT false,
+    CreateDate timestamp NOT NULL DEFAULT NOW(),
+	CreatorId bigint NOT NULL,
+    ModifyDate timestamp,
+	ModifierId bigint
+);
+
+CREATE TABLE Basic.Status(
+    Id serial PRIMARY KEY NOT NULL, 
+    Code varchar(10) NOT NULL UNIQUE,
+    Title varchar(200) NOT NULL,
+	Color varchar(7) NOT NULL,
+	IsToDo boolean NOT NULL,
+	IsInProgress boolean NOT NULL,
+	IsDone boolean NOT NULL,
+	Description varchar(4000),
+    IsActive boolean NOT NULL DEFAULT true,
+    IsDeleted boolean NOT NULL DEFAULT false,
+    CreateDate timestamp NOT NULL DEFAULT NOW(),
+	CreatorId bigint NOT NULL,
+    ModifyDate timestamp,
+	ModifierId bigint
+);
+
+CREATE TABLE Basic.Resolvation(
+    Id serial PRIMARY KEY NOT NULL, 
+    Code varchar(10) NOT NULL UNIQUE,
+    Title varchar(200) NOT NULL,
+	Color varchar(7) NOT NULL,
+	IsResolved boolean NOT NULL,
 	Description varchar(4000),
     IsActive boolean NOT NULL DEFAULT true,
     IsDeleted boolean NOT NULL DEFAULT false,
@@ -70,6 +119,20 @@ CREATE TABLE Basic.ContractType(
     Id serial PRIMARY KEY NOT NULL, 
     Code varchar(10) NOT NULL UNIQUE,
     Title varchar(200) NOT NULL,
+	Description varchar(4000),
+    IsActive boolean NOT NULL DEFAULT true,
+    IsDeleted boolean NOT NULL DEFAULT false,
+    CreateDate timestamp NOT NULL DEFAULT NOW(),
+	CreatorId bigint NOT NULL,
+    ModifyDate timestamp,
+	ModifierId bigint
+);
+
+CREATE TABLE Basic.TimeUnit(
+    Id serial PRIMARY KEY NOT NULL, 
+    Code varchar(10) NOT NULL UNIQUE,
+    Title varchar(200) NOT NULL,
+	BasedOnMiliscond bigint NOT NULL,
 	Description varchar(4000),
     IsActive boolean NOT NULL DEFAULT true,
     IsDeleted boolean NOT NULL DEFAULT false,
